d1889d6ff3d5f3fa87e8cf5dbc667a7a3a538783
culv <39304490+culv@users.noreply.github.com>
2024-04-06 13:20:14 -0400

    formatting

--- ops.py
+++ ops.py
@@ -4,20 +4,21 @@ from typing import Optional
 import torch
 from torch.nn import functional as F
 
+
 def scaled_dot_product_attention(
     query: torch.Tensor,
     key: torch.Tensor,
     value: torch.Tensor,
-    mask: Optional[torch.Tensor] = None
+    mask: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
     """Compute scaled dot product attention for the given key/query/value.
-    
+
     Args:
         query (torch.Tensor): Tensor with shape [..., L, d_query] where L is the length of the sequence
         key (torch.Tensor): Tensor with shape [..., L, d_key]
         value (torch.Tensor): Tensor with shape [..., L, d_value]
         mask (torch.Tensor): Optional mask to apply to attention scores (with shape [..., L, L])
-    
+
     Returns:
         output: The final result of scaled dot product attention
         attention: A [..., L, L] tensor representing the attention
@@ -25,14 +26,15 @@ def scaled_dot_product_attention(
     d_k = key.shape[-1]
     attention = torch.matmul(query, key.transpose(-1, -2))
     attention /= math.sqrt(d_k)
-    
+
     if mask is not None:
-        attention = attention.masked_fill(mask==0, -1e9)
-    
+        attention = attention.masked_fill(mask == 0, -1e9)
+
     attention = F.softmax(attention, dim=-1)
     output = torch.matmul(attention, value)
-    
+
     return output, attention
+
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
